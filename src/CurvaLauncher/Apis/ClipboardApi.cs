30e91c944cd6d28d41fde3d891e8abee1fac5a6f
小仙女 <me@ilyfairy.com>
2024-01-20 21:52:19 +0800

    feat: 添加了剪切板图像复制

--- src/CurvaLauncher/Apis/ClipboardApi.cs
+++ src/CurvaLauncher/Apis/ClipboardApi.cs
@@ -2,15 +2,14 @@
 using System.Windows.Media;
 using CurvaLauncher.Utilities;
 
-namespace CurvaLauncher.Apis
-{
-    public class ClipboardApi : IClipboardApi
-    {
-        private ClipboardApi() { }
-        public static ClipboardApi Instance { get; } = new();
+namespace CurvaLauncher.Apis;
 
-        public void SetImage(ImageSource image) => ClipboardUtils.SetBitmap(image);
-        public void SetImage(Image image) => ClipboardUtils.SetBitmap(image);
-        public void SetText(string text) => ClipboardUtils.SetText(text);
-    }
+public class ClipboardApi : IClipboardApi
+{
+    private ClipboardApi() { }
+    public static ClipboardApi Instance { get; } = new();
+
+    public void SetImage(ImageSource image) => ClipboardUtils.SetBitmap(image);
+    public void SetImage(Image image) => ClipboardUtils.SetBitmap(image);
+    public void SetText(string text) => ClipboardUtils.SetText(text);
 }
